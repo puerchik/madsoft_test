f38d4e8be8a2aa71910fcb5c3e94ef2588257785
puerchik <rumyantsevnn@yandex.ru>
2024-10-18 09:50:13 +0300

    fix: fix styles

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,5 +1,12 @@
+import { TestCreationForm } from '../testCreationForm'
+import { TestSelectionComponent } from '../testSelectionComponent'
 import s from './homePage.module.scss'
 
 export const HomePage = () => {
-  return <div className={s.wrapper}></div>
+  return (
+    <div className={s.wrapper}>
+      <TestSelectionComponent />
+      <TestCreationForm />
+    </div>
+  )
 }
--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,20 +1,27 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { App } from '@/app/App'
 import { Question } from '@/components/question'
 import { Result } from '@/components/result'
+import { App } from '@/app/App'
+import { HomePage } from '@/components/homePage'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-  },
-  {
-    path: '/test/:id/:number',
-    element: <Question />,
-  },
-  {
-    path: '/test/:id/result',
-    element: <Result />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/test/:id/:number',
+        element: <Question />,
+      },
+      {
+        path: '/test/:id/result',
+        element: <Result />,
+      },
+    ],
   },
 ])
